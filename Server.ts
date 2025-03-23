d53b4a5aae1c2b8f16014a9537f33599fab63abd
USER <sithiraroneth@gmail.com>
2025-03-23 13:00:30 +0530

    add router path to user

--- Server.ts
+++ Server.ts
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import UserController from "./controller/UserController";
 
 const app = express();
+const mongoUrl = 'mongodb://localhost:27017/ThirstyFirst';
+
+mongoose
+    .connect(mongoUrl)
+    .then(()=>console.log("MongoDB Connected Successfully"))
+    .catch((err:any)=> console.log("MongoDB Connection Error :",err))
 
 app.use(express.json());
+
+app.use('/user',UserController);
 
 app.listen(3000,()=>{
     console.log("Server running port 3000")
