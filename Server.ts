f23c2a5d26d22a9d74279eea1ef23c50b0aa3612
USER <sithiraroneth@gmail.com>
2025-03-24 12:08:18 +0530

    add cors

--- Server.ts
+++ Server.ts
@@ -5,6 +5,12 @@ import UserController from "./controller/UserController";
 const app = express();
 const mongoUrl = 'mongodb://localhost:27017/ThirstyFirst';
 
+app.use('/',(req,res, next)=>{
+    res.header('Access-Control-Allow-Origin',"*");
+    res.header('Access-Control-Allow-Methods',"GET,PUT,POST,DELETE,OPTIONS");
+    res.header('Access-Control-Allow-Headers',"Origin,X-Requested-With, Content-Type, Accept");
+    next();
+});
 mongoose
     .connect(mongoUrl)
     .then(()=>console.log("MongoDB Connected Successfully"))
